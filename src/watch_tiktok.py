bcf16ba513c94b6e0a613d9524604c13fb6e65b0
Adrian Lehmann <info@adrianlehmann.net>
2022-02-11 13:35:27 -0600

    Working version of watch_tiktok

--- src/watch_tiktok.py
+++ src/watch_tiktok.py
@@ -2,25 +2,41 @@ import random
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 def watch_and_continue(start_url, se_driver: webdriver, no_videos, like_prob=0):
     url = start_url
+    se_driver.get(url)
     urls = [start_url]
     for i in range(no_videos):
         like = random.uniform(0, 1) < like_prob
-        url = watch_tiktok(url, se_driver, like)
+        url = _watch_tiktok(url, se_driver, like)
         urls.append(url)
     return urls
 
 
 def watch_tiktok(url, se_driver: webdriver, like=False):
     se_driver.get(url)
+    return _watch_tiktok(url, se_driver, like)
+
+
+def _watch_tiktok(url, se_driver: webdriver, like=False):
     _wait_tiktok_to_load(se_driver)
-    time.sleep(_get_duration(se_driver) + 2)  # wait until video has certainly played
+    print("Loaded " + url)
+    duration = _get_duration(se_driver)
+    while duration is None:
+        duration = _get_duration(se_driver)
+    print(duration)
+    time.sleep(duration)  # wait until video has certainly played
+    chain = ActionChains(driver=se_driver)
     if like:  # Then maybe interact
-        se_driver.send_keys('L')
-    se_driver.send_keys(Keys.DOWN)
+        chain.send_keys('L')
+        chain.pause(0.6)
+    print("Next...")
+    chain.send_keys(Keys.SPACE)
+    chain.perform()
+    time.sleep(1)
     return se_driver.current_url
 
 
@@ -35,5 +51,5 @@ def _get_video(se_driver: webdriver):
     return se_driver.find_element_by_css_selector("[class*='VideoBasic']")
 
 
-def _get_duration(se_driver: webdriver):
-    return se_driver.execute_script("document.querySelectorAll('[class*=\"VideoBasic\"]')[0].duration")
+def _get_duration(se_driver: webdriver):  # Legacy / fallback when api does not work
+    return se_driver.execute_script("return document.querySelectorAll('[class*=\"VideoBasic\"]')[0].duration")
